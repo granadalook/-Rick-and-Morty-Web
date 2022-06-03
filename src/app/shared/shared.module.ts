d54c47f50fb9e950c421d5fde68bbb095fa96fdf
granadalook <tiven17.jsga@gmail.com>
2022-06-03 09:36:35 -0500

    CREATION: Se crean los componentes footer y header en el modulo  shared.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './components/Header/header/header.component';
+import { FooterComponent } from './components/Footer/footer/footer.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent, FooterComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
